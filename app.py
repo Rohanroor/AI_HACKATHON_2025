9dfdf29476e2767e51d98e823f989af8cdd706b6
bghk9495@gmail.com <harekrishnabagh14@gmail.com>
2025-02-19 23:46:01 +0530

    Added Map fuctionality

--- app.py
+++ app.py
@@ -143,5 +143,103 @@ def predict():
     # GET request - show form
     return render_template('diabetes.html')
 
+@app.route('/search-facilities')
+def search_facilities():
+    return render_template('search_facilities.html')
+
+@app.route('/get-nearby-facilities', methods=['POST'])
+def get_nearby_facilities():
+    try:
+        # Real facilities in Sambalpur
+        sample_facilities = [
+            {
+                "name": "Veer Surendra Sai Institute of Medical Sciences and Research (VIMSAR)",
+                "type": "Hospital",
+                "address": "Burla, Sambalpur, Odisha 768017",
+                "distance": "0.5 km",
+                "rating": 4.1
+            },
+            {
+                "name": "District Headquarters Hospital",
+                "type": "Hospital",
+                "address": "Church Rd, Naya Para, Sambalpur, Odisha 768001",
+                "distance": "1.2 km",
+                "rating": 3.9
+            },
+            {
+                "name": "Sambalpur Diagnostic Centre",
+                "type": "Pathlab",
+                "address": "Budharaja, Sambalpur, Odisha 768004",
+                "distance": "2.1 km",
+                "rating": 4.3
+            },
+            {
+                "name": "Lifeline Diagnostics & Research Centre",
+                "type": "Pathlab",
+                "address": "Near SBI Bank, Budharaja, Sambalpur, Odisha 768004",
+                "distance": "2.3 km",
+                "rating": 4.0
+            },
+            {
+                "name": "Aditya Care Hospital",
+                "type": "Hospital",
+                "address": "NH 6, Modipara, Sambalpur, Odisha 768002",
+                "distance": "3.0 km",
+                "rating": 4.2
+            },
+            {
+                "name": "Sambalpur Cancer Care Hospital",
+                "type": "Hospital",
+                "address": "Near Ainthapali Chowk, Sambalpur, Odisha 768004",
+                "distance": "3.5 km",
+                "rating": 4.4
+            },
+            {
+                "name": "City Care Hospital",
+                "type": "Hospital",
+                "address": "Budharaja, Sambalpur, Odisha 768004",
+                "distance": "2.8 km",
+                "rating": 4.0
+            },
+            {
+                "name": "Hitech Diagnostic Centre",
+                "type": "Pathlab",
+                "address": "Modipara, Sambalpur, Odisha 768002",
+                "distance": "2.5 km",
+                "rating": 4.2
+            },
+            {
+                "name": "Apex Diagnostics",
+                "type": "Pathlab",
+                "address": "Near Golbazar, Sambalpur, Odisha 768001",
+                "distance": "1.8 km",
+                "rating": 4.1
+            },
+            {
+                "name": "Ayush Hospital",
+                "type": "Hospital",
+                "address": "Dhanupali, Sambalpur, Odisha 768001",
+                "distance": "2.0 km",
+                "rating": 3.8
+            },
+            {
+                "name": "Kalinga Laboratory",
+                "type": "Pathlab",
+                "address": "Near Court Complex, Sambalpur, Odisha 768001",
+                "distance": "1.5 km",
+                "rating": 4.0
+            },
+            {
+                "name": "Sun Hospital",
+                "type": "Hospital",
+                "address": "Budharaja, Sambalpur, Odisha 768004",
+                "distance": "2.7 km",
+                "rating": 3.9
+            }
+        ]
+        return jsonify({"facilities": sample_facilities})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
